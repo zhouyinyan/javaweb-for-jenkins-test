a997b36699bab13bef801fe420fd2e9b1b14721d
zhouyinyan <gruelac@126.com>
2017-02-24 20:59:29 +0800

    init

---
+++ src/main/java/com/zyy/test/JenkinstestApplication.java
@@ -0,0 +1,12 @@
+package com.zyy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstestApplication.class, args);
+	}
+}
